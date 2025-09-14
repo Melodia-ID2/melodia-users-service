7883ac65d0c5fbc9fd87050069d5c3079bff5b7b
AizenSanchez <asancheza@fi.uba.ar>
2025-09-14 15:37:40 -0300

    test: add tests for update user roles from backoffice

---
+++ tests/integral/test_patch_role_user.py
@@ -0,0 +1,83 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+from app.core.security import get_jwt_payload, require_admin
+from app.models.user import UserAccount, UserProfile
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import uuid
+
+from app.core.config import settings
+sync_engine = create_engine(settings.DATABASE_URL.replace('+asyncpg', ''))
+
+def override_require_admin():
+    return None
+
+def create_user_with_role(role: str) -> uuid.UUID:
+    user_id = uuid.uuid4()
+    with Session(sync_engine) as session:
+        user = UserAccount(id=user_id, email="test@example.com", password="password")
+        user_profile = UserProfile(id=user_id, role=role)
+        session.add(user)
+        session.add(user_profile)
+
+        session.commit()
+    return user_id
+
+def test_01_patch_user_with_listener_role_to_artist():
+    app.dependency_overrides[require_admin] = override_require_admin
+    user_id = create_user_with_role("listener")
+    client = TestClient(app)
+    headers = {"Authorization": "Bearer admin_token"}
+    response = client.patch(f"/users/{user_id}/role", headers=headers)
+
+    assert response.status_code == 200
+    assert response.json()=={
+        "user": {
+            "id": str(user_id),
+            "email": "test@example.com",
+            "username": None,
+            "role": "artist"
+        }
+    }
+    with Session(sync_engine) as session:
+        user_profile = session.get(UserProfile, user_id)
+        assert user_profile.role == "artist"
+    app.dependency_overrides = {}
+
+def test_02_patch_user_with_artist_role_to_listener():
+    app.dependency_overrides[require_admin] = override_require_admin
+    user_id = create_user_with_role("artist")
+    client = TestClient(app)
+    headers = {"Authorization": "Bearer admin_token"}
+    response = client.patch(f"/users/{user_id}/role", headers=headers)
+
+    assert response.status_code == 200
+    assert response.json()=={
+        "user": {
+            "id": str(user_id),
+            "email": "test@example.com",
+            "username": None,
+            "role": "listener"
+        }
+    }
+    with Session(sync_engine) as session:
+        user_profile = session.get(UserProfile, user_id)
+        assert user_profile.role == "listener"
+    app.dependency_overrides = {}
+
+def test_03_patch_user_with_non_existent_id_returns_404():
+    app.dependency_overrides[require_admin] = override_require_admin
+    client = TestClient(app)
+    user_id = uuid.uuid4()
+    headers = {"Authorization": "Bearer admin_token"}
+    response = client.patch(f"/users/{user_id}/role", headers=headers)
+
+    assert response.status_code == 404
+    assert response.json() == {
+        "type": "about:blank",
+        "title": "Resource Not Found",
+        "status": 404,
+        "detail": f"User with id: {user_id} not found",
+        "instance": f"/users/{user_id}/role"
+    }
