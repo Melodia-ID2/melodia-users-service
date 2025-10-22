4457bb66ef196494fed656ea4dbcc29eb27626bc
Francisco Lopez Tancredi <flopezt@fi.uba.ar>
2025-10-22 12:22:41 -0300

    feat!: implement user credential model

---
+++ app/models/usercredential.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+
+from sqlmodel import Field, SQLModel
+
+
+class UserCredential(SQLModel, table=True):
+    email: str = Field(primary_key=True)
+    provider: str = Field(primary_key=True, default="local")
+    password: str | None = Field(default=None)
+    provider_id: str | None = Field(default=None)
+    user_id: UUID = Field(foreign_key="useraccount.id", index=True)
